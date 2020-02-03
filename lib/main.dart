528d424530fed2b0e2c5b705823f37044918b680
fabiotph <fabiotperez@gmail.com>
2020-02-02 22:05:40 -0300

    add and show TaskList

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_list/models/task.dart';
 
 void main() => runApp(App());
 
@@ -15,15 +16,25 @@ class App extends StatelessWidget {
       darkTheme:
           ThemeData(primaryColor: Colors.amber, brightness: Brightness.dark),
       themeMode: ThemeMode.dark,
-      showPerformanceOverlay: false,
-      debugShowCheckedModeBanner: false,
-      debugShowMaterialGrid: false,
-      showSemanticsDebugger: false,
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var tasks = new List<Task>();
+
+  HomePage() {
+    tasks = [];
+    tasks.add(Task(title: "Tarefa 1", done: false));
+    tasks.add(Task(title: "Tarefa 2", done: true));
+    tasks.add(Task(title: "Tarefa 3", done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,11 +42,18 @@ class HomePage extends StatelessWidget {
         leading: Icon(Icons.menu),
         title: Text("TODO List"),
       ),
-      body: Container(
-        child: Center(
-          child: Text("Hello World"),
-        ),
-      ),
+      body: ListView.builder(
+          itemCount: widget.tasks.length,
+          itemBuilder: (BuildContext ctx, int index) {
+            final task = widget.tasks[index];
+            return CheckboxListTile(
+                title: Text(task.title),
+                key: Key(task.id.toString()),
+                value: task.done,
+                onChanged: null,
+                activeColor: Colors.amber,
+                subtitle: Text(task.subtitle));
+          }),
     );
   }
 }
---
+++ lib/models/task.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+class Task {
+  DateTime id = new DateTime.now();
+  String title;
+  String subtitle;
+  bool done;
+
+  Task({this.title, this.subtitle = "", this.done});
+}
